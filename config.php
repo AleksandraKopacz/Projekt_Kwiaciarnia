5f768887b4890e94cc8ed2c6af44995a15053a36
AleksandraKopacz <aleksandrakopacz4@gmail.com>
2023-05-22 18:21:57 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+$conf->debug = true;
+
+$conf->root_path = dirname(__FILE__);
+$conf->server_name = 'localhost';
+$conf->server_url = 'http://' . $conf->server_name;
+$conf->app_root = '/Kwiaciarnia';
+$conf->app_url = $conf->server_url . $conf->app_root;
+$conf->action_root = $conf->app_root . '/ctrl.php?action=';
+$conf->action_url = $conf->server_url . $conf->action_root;
+
+$conf->db_type = 'mysql';
+$conf->db_server = 'localhost';
+$conf->db_name = 'kwiaciarnia';
+$conf->db_user = 'root';
+$conf->db_pass = '';
+$conf->db_charset = 'utf8';
+
+$conf->db_port = '3306';
+$conf->db_option = [PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
